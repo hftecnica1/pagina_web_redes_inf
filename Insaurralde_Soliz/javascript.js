712976150a8beacdf508b6a75b4bd5eed0281c50
Carlos Soliz <carlos.soliz.t1vl@gmail.com>
2022-08-02 16:47:45 -0300

    carlos

--- Insaurralde_Soliz/javascript.js
+++ Insaurralde_Soliz/javascript.js
@@ -2,6 +2,6 @@ const adios = document.querySelector(".adios")
 const hola = document.querySelector(".hola")
 
 adios.addEventListener("mousemove",(x)=>{
-    x.target.classList.value =="adios" ?  alert("perdiste"):"hola"
+    x.target.classList.value =="adios" ?  console.log("perdiste"):"hola"
 })
 
