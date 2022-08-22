26945553f0cc96b2f2a9fd54a1b509e28f78d6e2
Carlos Soliz <carlos.soliz.t1vl@gmail.com>
2022-08-22 16:33:02 -0300

    carlos

--- Insaurralde_Soliz/javascript.js
+++ Insaurralde_Soliz/javascript.js
@@ -1,7 +1,21 @@
 const contenedor =  document.getElementById("cont");
 const camino = document.getElementById("cmn");
+const comenzar = document.getElementById( "cmzr" );
+const mensaje = document.getElementById( "msj" );
+
+let metas = "meta1"
 
 contenedor.addEventListener("mousemove",(x)=>{
-    x.target.classList.value =="container" ?  alert("perdiste"):"" 
+    x.target.classList.value == "cuerpo" ?  perdiste():"" 
+    x.target.classList.value == metas ? console.log(1):1 
 })
 
+function perdiste( ) {
+    mensaje.innerText="reintentar"
+    comenzar.style.zIndex=1;
+}
+function empezar( ) {
+    metas = "meta1"
+    camino.classList.value = "camino1"
+    comenzar.style.zIndex=-1;
+}
